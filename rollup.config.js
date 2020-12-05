c91b18993531e454245e8cb4dffe255ea9efb4a1
Gu-Miao <1450206741@qq.com>
2020-12-05 22:21:18 +0800

    add clear plugin

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import eslint from '@rollup/plugin-eslint'
 import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
+import clear from 'rollup-plugin-clear'
 import pkg from './package.json'
 
 const name = 'GmUtils'
@@ -36,6 +37,7 @@ export default {
     }
   ],
   plugins: [
+    clear({ targets: ['dist'] }),
     resolve(),
     commonjs(),
     eslint({ exclude: 'node_modules' }),
