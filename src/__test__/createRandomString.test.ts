4aea9dd608043ddd96d99112247303a5b49219e6
Gu-Miao <1450206741@qq.com>
2021-10-03 18:57:26 +0800

    When length equals 0, returns empty string.

--- src/__test__/createRandomString.test.ts
+++ src/__test__/createRandomString.test.ts
@@ -12,7 +12,7 @@ describe('Generate correct random string', () => {
   })
 
   test('Return only includes letters and numbers', () => {
-    const regexp = /^[a-zA-Z0-9]+$/
+    const regexp = /^[a-zA-Z0-9]*$/
     for (let i = 0; i < times; i++) {
       const length = Math.round(Math.random() * 1000)
       const string = createRandomString(length)
