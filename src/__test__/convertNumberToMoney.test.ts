d1bf414207345867c00dd947fd86471c0b3ec6ab
Gu-Miao <1450206741@qq.com>
2021-10-08 13:48:02 +0800

    Update money.ts and its test.

--- src/__test__/convertNumberToMoney.test.ts
+++ src/__test__/convertNumberToMoney.test.ts
@@ -1,19 +1,46 @@
 import { convertNumberToMoney } from '../money'
-import { createRandomInteger } from '../createRandom'
-import { isString } from '../type'
-
-const times = 100
 
 describe('Generate correct money string with thousand mark', () => {
-  test('Return string', () => {
-    for (let i = 0; i < times; i++) {
-      const negative = i % 3 ? 1 : -1
-      const number = createRandomInteger(20000)
-      let money: string | number = number * negative
-      if (i % 2) {
-        money = money.toString()
-      }
-      expect(isString(convertNumberToMoney(money))).toBeTruthy()
-    }
+  test('Pass illegal string as first argument', () => {
+    expect(() => convertNumberToMoney('')).toThrow()
+    expect(() => convertNumberToMoney('true')).toThrow()
+    expect(() => convertNumberToMoney('false')).toThrow()
+    expect(() => convertNumberToMoney('asdfqwerf')).toThrow()
+    expect(() => convertNumberToMoney('123.123{asdf)')).toThrow()
+  })
+
+  test('Pass NaN as first argument', () => {
+    expect(() => convertNumberToMoney(NaN)).toThrow()
+    expect(() => convertNumberToMoney('NaN')).toThrow()
+  })
+
+  test('Pass Infinity as first argument', () => {
+    expect(() => convertNumberToMoney(Infinity)).toThrow()
+    expect(() => convertNumberToMoney('Infinity')).toThrow()
+  })
+
+  test('Pass number equal or less than 0 as first argument', () => {
+    expect(() => convertNumberToMoney(0)).toThrow()
+    expect(() => convertNumberToMoney(-1)).toThrow()
+    expect(() => convertNumberToMoney(-100e2)).toThrow()
+    expect(() => convertNumberToMoney(-Infinity)).toThrow()
+    expect(() => convertNumberToMoney('0')).toThrow()
+    expect(() => convertNumberToMoney('-1')).toThrow()
+    expect(() => convertNumberToMoney('-333e7')).toThrow()
+    expect(() => convertNumberToMoney('-Infinity')).toThrow()
+  })
+
+  test('Pass illegal as second argument', () => {
+    expect(() => convertNumberToMoney(1000, -1)).toThrow()
+    expect(() => convertNumberToMoney(1000, 30)).toThrow()
+    expect(() => convertNumberToMoney(1000, 10.5)).toThrow()
+  })
+
+  test('Return current format', () => {
+    const regexp = /^\d{1,3}(,\d{3})*\.\d{0,20}$/
+    expect(regexp.test(convertNumberToMoney(1000))).toBeTruthy()
+    expect(regexp.test(convertNumberToMoney(1e12))).toBeTruthy()
+    expect(regexp.test(convertNumberToMoney('1000'))).toBeTruthy()
+    expect(regexp.test(convertNumberToMoney('1e12'))).toBeTruthy()
   })
 })
--- src/money.ts
+++ src/money.ts
@@ -4,12 +4,37 @@ import { isString } from './type'
  * Convert number to money with thousand mark.
  *
  * @param number Money number.
+ * @param fractionDigits Decimal digits.
  * @return Money with thousand mark.
  */
-export function convertNumberToMoney(number: number | string): string {
-  const num = isString(number) ? parseFloat(number as string) : (number as number)
-  const negative = num < 0 ? '-' : ''
-  const float = Math.abs(num).toFixed(2)
+export function convertNumberToMoney(number: number | string, fractionDigits = 2): string {
+  const isStringArg = isString(number)
+
+  // Support decimals and scientific notation.
+  const regexp = /^\d+(\.\d+)?(e\d+)?$/
+  if (isStringArg && !regexp.test(number as string)) {
+    throw new SyntaxError(`The first argument must be legal number, but got ${number}`)
+  }
+
+  const num = isStringArg ? parseFloat(number as string) : (number as number)
+
+  // number is not be NaN, Infinity and should be positive.
+  if (isNaN(num)) {
+    throw new TypeError(`The first argument must be legal number, but got 'NaN'.`)
+  }
+  if (!isFinite(num)) {
+    throw new TypeError(`The first argument must be legal number, but got 'Infinity'.`)
+  }
+  if (num <= 0) {
+    throw new TypeError(`The first argument must be a positive number.`)
+  }
+
+  // `fractionDigits` should be 0~20 integer.
+  if (fractionDigits < 0 || fractionDigits > 20 || !Number.isInteger(fractionDigits)) {
+    throw new TypeError(`The second argument must be a integer and in range [0, 20].`)
+  }
+
+  const float = num.toFixed(fractionDigits)
   const [intPart, floatPart] = float.split('.')
   const intPartArray = Array.from(intPart)
 
@@ -17,9 +42,9 @@ export function convertNumberToMoney(number: number | string): string {
   let parsedInt = ''
 
   while (i--) {
-    const len = intPartArray.length
+    const { length } = intPartArray
 
-    if ((len - i) % 3 === 0 && len > 3) {
+    if ((length - i) % 3 === 0 && length > 3) {
       parsedInt = `,${intPartArray.splice(i, 3).join('')}${parsedInt}`
     }
     if (!i) {
@@ -27,5 +52,5 @@ export function convertNumberToMoney(number: number | string): string {
     }
   }
 
-  return `${negative}${parsedInt}.${floatPart}`
+  return `${parsedInt}.${floatPart}`
 }
